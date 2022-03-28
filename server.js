7f4a19dd1267c0f3db548473ceb9d33fc362e73b
briangor <briangor13@gmail.com>
2022-03-28 21:02:33 +0300

    Update

--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/angular-app'));
 
-app.get('/*', function(req,res) {
+app.get('/*', (req,res) => {
     res.sendFile(path.join(__dirname+'/dist/angular-app/index.html'));
+    //res.sendFile('index.html', {root: 'dist/angular-app/'});
 });
 
 app.listen(process.env.PORT || 8080);
